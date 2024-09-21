0d60f25af33689cc4e0b9c77ff9031318f32a5fd
Silvanos Eric <ericoderito@gmail.com>
2024-09-21 16:19:58 +0300

    feat(models): add methods to Venue model for concert lookup and band frequency
    - Import datetime module
    - Add `concert_on` method to find a concert on a specific date
    - Add `most_frequent_band` method to return the band with the most concerts at the venue

--- app/models/venue.py
+++ app/models/venue.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
+from datetime import datetime
 from .base import Base
 
 
@@ -14,3 +15,19 @@ class Venue(Base):
 
     def __repr__(self):
         return f"Venue(id={self.id}, title={self.title}, city={self.city})"
+
+    def concert_on(self, date):
+        return next(
+            (concert for concert in self.concerts
+             if concert.date == datetime.strptime(date, "%Y-%m-%d").date()),
+            None)
+
+    def most_frequent_band(self):
+        """Returns the band with the most concerts at this venue."""
+        band_counts = {}
+        for concert in self.concerts:
+            band = concert.band
+            if band not in band_counts:
+                band_counts[band] = 0
+            band_counts[band] += 1
+        return max(band_counts, key=band_counts.get)
